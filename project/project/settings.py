c028807d79ea8a24749901ee2e12f2a49f20ed84
Oleg Matveenko <alecnin@gmail.com>
2020-05-28 22:17:35 -0700

    added homepage

--- project/project/settings.py
+++ project/project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'project/templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
