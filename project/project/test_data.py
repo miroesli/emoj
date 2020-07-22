7a375a379371cabc0a45e57eddaff83761c62584
Juan James Comish <jjcomish@shaw.ca>
2020-07-22 13:48:53 -0700

    Added test cards to test deck in test_data.py

--- project/project/test_data.py
+++ project/project/test_data.py
@@ -7,3 +7,8 @@ for suit in suits:
     for rank in ranks:
         db_handler.insert_card(rank + " of " + suit, None, rank + "_" + suit)
 
+cards = db_handler.get_cards()
+decks = db_handler.get_template_decks("36e3531e-cc52-11ea-b508-784f437b7506")
+for deck in decks:
+    for card in cards:
+        db_handler.insert_card_into_deck(deck.deck_uid, card.card_uid)
