19f0d4b820cbce62089b0a9976a4a02dd9712e2b
Juan James Comish <jjcomish@shaw.ca>
2020-07-22 22:21:08 -0700

    Laying out path from axios request to db_handler
    Will need to clarify this should work with someone

--- project/project/views/api.py
+++ project/project/views/api.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+import utils.py
 
 
 def id(request, id):
     return HttpResponse("You're looking at id %s." % id)
+
+def option(request, data):
+    return utils.function_handler(request.body)
+# TODO: add/modify a function to call utils with all the backend passing.
+# include the necessary data as a json payload?
+# Should be in the request.body?
+# https://stackoverflow.com/questions/45072255/axios-post-array-data
+# possibly have the different functions return different HttpResponse.status_code HttpReponse.content depending on it's error handling
