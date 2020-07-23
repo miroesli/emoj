7d04c5a591f932d5f40ebd70bc729e6254690f2a
Oleg Matveenko <alecnin@gmail.com>
2020-07-23 14:38:02 -0700

    tranfer function outline

--- project/project/utils.py
+++ project/project/utils.py
@@ -10,3 +10,12 @@ def deal_card(room_uid, player_uid, game_board_location):
     card_uids = db_handler.draw_game_board_location_top_card(room_uid, game_board_location)
     card_uid = random.choice(card_uids)
     db_handler.update_card_location(room_uid, card_uid, None, player_uid)
+
+
+def transfer_card(entity_type1,entity_type2,entity_uid1,entity_uid2):
+    if entity_type1 not in ['card','player','location']:
+        pass #throws err
+    if entity_type2 not in ['card','player','location']:
+        pass #throws err
+    else:
+        pass #do stuff
