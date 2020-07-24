634ed1543508bd784d6b620c4c47250e135e877c
Oleg Matveenko <alecnin@gmail.com>
2020-07-23 20:30:24 -0700

    slight change to api call

--- project/project/views/api.py
+++ project/project/views/api.py
@@ -11,10 +11,11 @@ def id(request, id):
 def option(request):
     # An action has been submitted and needs to be processed.
     if(request.method == 'POST'):
-        room_uid = request.GET.get('room_uid')
-        player_uid = request.GET.get('player_uid')
-        selected = json.loads(request.GET.get('selected'))
-        return json.dumps(utils.function_handler(room_uid, player_uid, selected))
+        room_uid = request.POST.get('room_uid')
+        player_uid = request.POST.get('player_uid')
+        option = request.POST.get('option')
+        selected = json.loads(request.POST.get('selected'))
+        return json.dumps(utils.function_handler(room_uid, player_uid,option, selected))
 
     # An item has been selected and the possible options need to be displayed.
     elif(request.method == 'GET'):
