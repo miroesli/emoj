f808f6097db06b9e459eeca5008fc9e12d80e86a
Michail Roesli <misha.roesli@gmail.com>
2020-07-26 22:22:41 -0700

    create docker entrypoint init for web

---
+++ project/docker/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Collect static files
+# echo "Collect static files"
+# python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Make database migrations"
+python manage.py makemigrations
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate --no-input
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
